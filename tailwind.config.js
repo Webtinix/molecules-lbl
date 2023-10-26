1dda3813a493662789625e2b11c76b97c3abe660
DaphNZITA <dnzita4@gmail.com>
2023-10-26 18:40:52 +0200

    taf sur les molecules forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lbl-primary': "#043570",
+         'lbl-secondary': "#C10C1A",
+         'lbl-black': "#000000",
+         'lbl-dark': "#1B1D22",
+         'lbl-white': "#FFFFFF",
+         'lbl-light': "#F5F5F5",
+         'lbl-error': "#FE414D",
+         'lbl-warning': "#FF9900",
+         'lbl-info': "#0051A2",
+         'lbl-success': "#98D02B"
+       }
+    },
   },
   plugins: [],
 }
